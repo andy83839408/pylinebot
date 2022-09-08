a72951e8028492a0791d6bdf9e41a5335df1a414
andy83839408 <40571627+andy83839408@users.noreply.github.com>
2022-09-08 14:49:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #載入LineBot所需要的模組
 from flask import Flask, request, abort
 from linebot import (
-　　LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-　　InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import *
 
@@ -23,28 +23,29 @@ line_bot_api.push_message('U62f7334ab2243374de92db45eab6e153', TextSendMessage(t
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-　　# get X-Line-Signature header value
-　　signature = request.headers['X-Line-Signature']
-　　# get request body as text
-　　body = request.get_data(as_text=True)
-　　app.logger.info("Request body: " + body)
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-　　# handle webhook body
-　　try:
-　　　　handler.handle(body, signature)
-　　except InvalidSignatureError:
-　　　　abort(400)
+    # handle webhook body
+    try:
+      handler.handle(body, signature)
+    except InvalidSignatureError:
+      abort(400)
 
-　　return 'OK'
+    return 'OK'
 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-　　message = TextSendMessage(text=event.message.text)
-　　line_bot_api.reply_message(event.reply_token,message)
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
-import os if __name__ == "__main__":
-　　port = int(os.environ.get('PORT', 5000))
-　　app.run(host='0.0.0.0', port=port)
+import os 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
