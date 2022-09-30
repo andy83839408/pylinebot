33c119f539ccec53fc06aff84a56f9e4c1c35058
andy83839408 <40571627+andy83839408@users.noreply.github.com>
2022-09-30 14:47:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -56,18 +56,38 @@ def handle_message(event):
     elif re.match("吃啥\s\+*",message):
       foodList.append(message.split("+")[1].strip())
       sendString="菜單已新增: "+message.split("+")[1].strip()
+    elif re.match("...天氣",message):
+      sendString=weatherReport(message.split("天氣")[0])
     else:
       sendString=message
     
-    line_bot_api.reply_message(
+    if sendString!="":
+      line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=sendString)
-    )
+      )
 
+#菜單變數
 foodList = ["大埔", "石二鍋", "薩利亞", "米粉湯", "炒飯", "烏龍麵"]
 import random
 def foodStraws():
     return foodList[random.randint(0, len(foodList) - 1)]
+
+import requests
+def weatherReport(loc="桃園市"):
+  url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-FE106A37-DA13-4687-9965-EE2C546A1C30'
+  data = requests.get(url)   # 取得 JSON 檔案的內容為文字
+  data_json = data.json()    # 轉換成 JSON 格式
+  location = data_json['records']['location']
+  for i in location:
+    if i['locationName']==loc:
+      city = i['locationName']    # 縣市名稱
+      wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
+      maxt8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最高溫
+      mint8 = i['weatherElement'][4]['time'][0]['parameter']['parameterName']  # 最低溫
+      ci8 = i['weatherElement'][3]['time'][0]['parameter']['parameterName']    # 舒適度
+      pop8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']   # 降雨機率
+      return f'{city}未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %'
 
 #主程式
 import os 
